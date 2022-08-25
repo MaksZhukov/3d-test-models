97ebe00fe24dc43cdcb13b3656992272f081b96f
MaksZhukov <maks_zhukov_97@mail.ru>
2022-08-25 17:38:12 +0300

    Added trigger points

--- src/index.ts
+++ src/index.ts
@@ -1,324 +1,390 @@
-import * as THREE from "three";
+import * as THREE from 'three';
 
-import Stats from "three/examples/jsm/libs/stats.module";
-import { GUI } from "lil-gui";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import Stats from 'three/examples/jsm/libs/stats.module';
+import { GUI } from 'lil-gui';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 let scene, renderer, camera, stats;
 let model, skeleton, mixer, clock;
 
 const crossFadeControls = [];
+let triggerPoints = [];
 
-let currentBaseAction = "idle";
+let currentBaseAction = 'idle';
 const allActions = [];
 const baseActions = {
-  idle: { weight: 1 },
-  walk: { weight: 0 },
-  run: { weight: 0 },
+	idle: { weight: 1 },
+	walk: { weight: 0 },
+	run: { weight: 0 },
 };
 const additiveActions = {
-  sneak_pose: { weight: 0 },
-  sad_pose: { weight: 0 },
-  agree: { weight: 0 },
-  headShake: { weight: 0 },
+	sneak_pose: { weight: 0 },
+	sad_pose: { weight: 0 },
+	agree: { weight: 0 },
+	headShake: { weight: 0 },
+};
+const myOwnActions = {
+	showTriggerPoints: true,
 };
 let panelSettings, numAnimations;
+
+const createTriggerPoint = (bodyPart, x, y, z) => {
+	const geometry = new THREE.SphereGeometry(3, 10, 10);
+	const material = new THREE.MeshBasicMaterial({ color: '#FF0000' });
+	material.depthTest = false;
+	const mesh = new THREE.Mesh(geometry, material);
+	mesh.position.set(x, y, z);
+	triggerPoints.push(mesh);
+	bodyPart.add(mesh);
+	console.log(bodyPart.dispose);
+	// bodyPart.geometry.dispose();
+	// bodyPart.material.dispose();
+	scene.remove(bodyPart);
+};
+
+const createTriggerPoints = () => {
+	const leftFoot = model.getObjectByName('mixamorigLeftFoot');
+	const rightFoot = model.getObjectByName('mixamorigRightFoot');
+	const leftHand = model.getObjectByName('mixamorigLeftHand');
+	const rightHand = model.getObjectByName('mixamorigRightHand');
+	createTriggerPoint(leftFoot, 0, 0, 0);
+	// leftFoot.clear();
+	createTriggerPoint(rightFoot, 0, 0, 0);
+	createTriggerPoint(leftHand, 0, 0, 2);
+	createTriggerPoint(rightHand, 0, 0, 2);
+};
 
 init();
 
 function init() {
-  const container = document.getElementById("container");
-  clock = new THREE.Clock();
+	const container = document.getElementById('container');
+	clock = new THREE.Clock();
 
-  scene = new THREE.Scene();
-  scene.background = new THREE.Color(0xa0a0a0);
-  scene.fog = new THREE.Fog(0xa0a0a0, 10, 50);
+	scene = new THREE.Scene();
+	scene.background = new THREE.Color(0xa0a0a0);
+	scene.fog = new THREE.Fog(0xa0a0a0, 10, 50);
 
-  const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
-  hemiLight.position.set(0, 20, 0);
-  scene.add(hemiLight);
+	const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+	hemiLight.position.set(0, 20, 0);
+	scene.add(hemiLight);
 
-  const dirLight = new THREE.DirectionalLight(0xffffff);
-  dirLight.position.set(3, 10, 10);
-  dirLight.castShadow = true;
-  dirLight.shadow.camera.top = 2;
-  dirLight.shadow.camera.bottom = -2;
-  dirLight.shadow.camera.left = -2;
-  dirLight.shadow.camera.right = 2;
-  dirLight.shadow.camera.near = 0.1;
-  dirLight.shadow.camera.far = 40;
-  scene.add(dirLight);
+	const dirLight = new THREE.DirectionalLight(0xffffff);
+	dirLight.position.set(3, 10, 10);
+	dirLight.castShadow = true;
+	dirLight.shadow.camera.top = 2;
+	dirLight.shadow.camera.bottom = -2;
+	dirLight.shadow.camera.left = -2;
+	dirLight.shadow.camera.right = 2;
+	dirLight.shadow.camera.near = 0.1;
+	dirLight.shadow.camera.far = 40;
+	scene.add(dirLight);
 
-  // ground
+	// ground
 
-  const mesh = new THREE.Mesh(
-    new THREE.PlaneGeometry(100, 100),
-    new THREE.MeshPhongMaterial({ color: 0x999999, depthWrite: false })
-  );
-  mesh.rotation.x = -Math.PI / 2;
-  mesh.receiveShadow = true;
-  scene.add(mesh);
+	const mesh = new THREE.Mesh(
+		new THREE.PlaneGeometry(100, 100),
+		new THREE.MeshPhongMaterial({ color: 0x999999, depthWrite: false })
+	);
+	mesh.rotation.x = -Math.PI / 2;
+	mesh.receiveShadow = true;
+	scene.add(mesh);
 
-  const loader = new GLTFLoader();
-  loader.load("Xbot.glb", function (gltf) {
-    model = gltf.scene;
-    console.log(gltf);
-    scene.add(model);
+	const axesHelper = new THREE.AxesHelper(5);
+	scene.add(axesHelper);
 
-    model.traverse(function (object) {
-      if (object.isMesh) object.castShadow = true;
-    });
+	const loader = new GLTFLoader();
+	loader.load('Xbot.glb', function (gltf) {
+		model = gltf.scene;
+		scene.add(model);
 
-    skeleton = new THREE.SkeletonHelper(model);
-    console.log(skeleton);
-    skeleton.visible = false;
-    scene.add(skeleton);
+		model.traverse(function (object) {
+			if (object.isMesh) {
+				object.castShadow = true;
+				scene.remove(object);
+			}
+			if (object.isSkinnedMesh) {
+				console.log(object);
+				// object.material.color = new THREE.Color('#000');
+				// object.visible = false;
+			}
+		});
 
-    const animations = gltf.animations;
-    mixer = new THREE.AnimationMixer(model);
+		console.log(gltf);
 
-    numAnimations = animations.length;
+		skeleton = new THREE.SkeletonHelper(model);
 
-    for (let i = 0; i !== numAnimations; ++i) {
-      let clip = animations[i];
-      const name = clip.name;
+		skeleton.visible = true;
+		scene.add(skeleton);
+		createTriggerPoints();
 
-      if (baseActions[name]) {
-        const action = mixer.clipAction(clip);
-        activateAction(action);
-        baseActions[name].action = action;
-        allActions.push(action);
-      } else if (additiveActions[name]) {
-        // Make the clip additive and remove the reference frame
+		const animations = gltf.animations;
+		mixer = new THREE.AnimationMixer(model);
 
-        THREE.AnimationUtils.makeClipAdditive(clip);
+		numAnimations = animations.length;
 
-        if (clip.name.endsWith("_pose")) {
-          clip = THREE.AnimationUtils.subclip(clip, clip.name, 2, 3, 30);
-        }
+		for (let i = 0; i !== numAnimations; ++i) {
+			let clip = animations[i];
+			const name = clip.name;
 
-        const action = mixer.clipAction(clip);
-        activateAction(action);
-        additiveActions[name].action = action;
-        allActions.push(action);
-      }
-    }
+			if (baseActions[name]) {
+				const action = mixer.clipAction(clip);
+				activateAction(action);
+				baseActions[name].action = action;
+				allActions.push(action);
+			} else if (additiveActions[name]) {
+				// Make the clip additive and remove the reference frame
 
-    createPanel();
+				THREE.AnimationUtils.makeClipAdditive(clip);
 
-    animate();
-  });
+				if (clip.name.endsWith('_pose')) {
+					clip = THREE.AnimationUtils.subclip(
+						clip,
+						clip.name,
+						2,
+						3,
+						30
+					);
+				}
 
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.outputEncoding = THREE.sRGBEncoding;
-  renderer.shadowMap.enabled = true;
-  container.appendChild(renderer.domElement);
+				const action = mixer.clipAction(clip);
+				activateAction(action);
+				additiveActions[name].action = action;
+				allActions.push(action);
+			}
+		}
 
-  // camera
-  camera = new THREE.PerspectiveCamera(
-    45,
-    window.innerWidth / window.innerHeight,
-    1,
-    100
-  );
-  camera.position.set(-1, 2, 3);
+		createPanel();
 
-  const controls = new OrbitControls(camera, renderer.domElement);
-  controls.enablePan = false;
-  controls.enableZoom = false;
-  controls.target.set(0, 1, 0);
-  controls.update();
+		animate();
+	});
 
-  stats = new Stats();
-  container.appendChild(stats.dom);
+	renderer = new THREE.WebGLRenderer({ antialias: true });
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.outputEncoding = THREE.sRGBEncoding;
+	renderer.shadowMap.enabled = true;
+	container.appendChild(renderer.domElement);
 
-  window.addEventListener("resize", onWindowResize);
+	// camera
+	camera = new THREE.PerspectiveCamera(
+		45,
+		window.innerWidth / window.innerHeight,
+		1,
+		100
+	);
+	camera.position.set(-1, 2, 3);
+
+	const controls = new OrbitControls(camera, renderer.domElement);
+	controls.enablePan = false;
+	controls.enableZoom = false;
+	controls.target.set(0, 1, 0);
+	controls.update();
+
+	stats = new Stats();
+	container.appendChild(stats.dom);
+
+	window.addEventListener('resize', onWindowResize);
 }
 
 function createPanel() {
-  const panel = new GUI({ width: 310 });
+	const panel = new GUI({ width: 310 });
 
-  const folder1 = panel.addFolder("Base Actions");
-  const folder2 = panel.addFolder("Additive Action Weights");
-  const folder3 = panel.addFolder("General Speed");
+	const folder1 = panel.addFolder('Base Actions');
+	const folder2 = panel.addFolder('Additive Action Weights');
+	const folder3 = panel.addFolder('General Speed');
+	const folder4 = panel.addFolder('My actions');
 
-  panelSettings = {
-    "modify time scale": 1.0,
-  };
+	panelSettings = {
+		'modify time scale': 1.0,
+	};
 
-  const baseNames = ["None", ...Object.keys(baseActions)];
+	const baseNames = ['None', ...Object.keys(baseActions)];
 
-  for (let i = 0, l = baseNames.length; i !== l; ++i) {
-    const name = baseNames[i];
-    const settings = baseActions[name];
-    panelSettings[name] = function () {
-      const currentSettings = baseActions[currentBaseAction];
-      const currentAction = currentSettings ? currentSettings.action : null;
-      const action = settings ? settings.action : null;
+	for (let i = 0, l = baseNames.length; i !== l; ++i) {
+		const name = baseNames[i];
+		const settings = baseActions[name];
+		panelSettings[name] = function () {
+			const currentSettings = baseActions[currentBaseAction];
+			const currentAction = currentSettings
+				? currentSettings.action
+				: null;
+			const action = settings ? settings.action : null;
 
-      if (currentAction !== action) {
-        prepareCrossFade(currentAction, action, 0.35);
-      }
-    };
+			if (currentAction !== action) {
+				prepareCrossFade(currentAction, action, 0.35);
+			}
+		};
 
-    crossFadeControls.push(folder1.add(panelSettings, name));
-  }
+		crossFadeControls.push(folder1.add(panelSettings, name));
+	}
 
-  for (const name of Object.keys(additiveActions)) {
-    const settings = additiveActions[name];
+	for (const name of Object.keys(additiveActions)) {
+		const settings = additiveActions[name];
 
-    panelSettings[name] = settings.weight;
-    folder2
-      .add(panelSettings, name, 0.0, 1.0, 0.01)
-      .listen()
-      .onChange(function (weight) {
-        setWeight(settings.action, weight);
-        settings.weight = weight;
-      });
-  }
+		panelSettings[name] = settings.weight;
+		folder2
+			.add(panelSettings, name, 0.0, 1.0, 0.01)
+			.listen()
+			.onChange(function (weight) {
+				setWeight(settings.action, weight);
+				settings.weight = weight;
+			});
+	}
 
-  folder3
-    .add(panelSettings, "modify time scale", 0.0, 1.5, 0.01)
-    .onChange(modifyTimeScale);
+	folder3
+		.add(panelSettings, 'modify time scale', 0.0, 1.5, 0.01)
+		.onChange(modifyTimeScale);
 
-  folder1.open();
-  folder2.open();
-  folder3.open();
+	for (const name of Object.keys(myOwnActions)) {
+		folder4.add(myOwnActions, name);
+	}
 
-  crossFadeControls.forEach(function (control) {
-    control.setInactive = function () {
-      control.domElement.classList.add("control-inactive");
-    };
+	folder1.open();
+	folder2.open();
+	folder3.open();
+	folder4.open();
 
-    control.setActive = function () {
-      control.domElement.classList.remove("control-inactive");
-    };
+	crossFadeControls.forEach(function (control) {
+		control.setInactive = function () {
+			control.domElement.classList.add('control-inactive');
+		};
 
-    const settings = baseActions[control.property];
+		control.setActive = function () {
+			control.domElement.classList.remove('control-inactive');
+		};
 
-    if (!settings || !settings.weight) {
-      control.setInactive();
-    }
-  });
+		const settings = baseActions[control.property];
+
+		if (!settings || !settings.weight) {
+			control.setInactive();
+		}
+	});
 }
 
 function activateAction(action) {
-  const clip = action.getClip();
-  const settings = baseActions[clip.name] || additiveActions[clip.name];
-  setWeight(action, settings.weight);
-  action.play();
+	const clip = action.getClip();
+	const settings = baseActions[clip.name] || additiveActions[clip.name];
+	setWeight(action, settings.weight);
+	action.play();
 }
 
 function modifyTimeScale(speed) {
-  mixer.timeScale = speed;
+	mixer.timeScale = speed;
 }
 
 function prepareCrossFade(startAction, endAction, duration) {
-  // If the current action is 'idle', execute the crossfade immediately;
-  // else wait until the current action has finished its current loop
+	// If the current action is 'idle', execute the crossfade immediately;
+	// else wait until the current action has finished its current loop
 
-  if (currentBaseAction === "idle" || !startAction || !endAction) {
-    executeCrossFade(startAction, endAction, duration);
-  } else {
-    synchronizeCrossFade(startAction, endAction, duration);
-  }
+	if (currentBaseAction === 'idle' || !startAction || !endAction) {
+		executeCrossFade(startAction, endAction, duration);
+	} else {
+		synchronizeCrossFade(startAction, endAction, duration);
+	}
 
-  // Update control colors
+	// Update control colors
 
-  if (endAction) {
-    const clip = endAction.getClip();
-    currentBaseAction = clip.name;
-  } else {
-    currentBaseAction = "None";
-  }
+	if (endAction) {
+		const clip = endAction.getClip();
+		currentBaseAction = clip.name;
+	} else {
+		currentBaseAction = 'None';
+	}
 
-  crossFadeControls.forEach(function (control) {
-    const name = control.property;
+	crossFadeControls.forEach(function (control) {
+		const name = control.property;
 
-    if (name === currentBaseAction) {
-      control.setActive();
-    } else {
-      control.setInactive();
-    }
-  });
+		if (name === currentBaseAction) {
+			control.setActive();
+		} else {
+			control.setInactive();
+		}
+	});
 }
 
 function synchronizeCrossFade(startAction, endAction, duration) {
-  mixer.addEventListener("loop", onLoopFinished);
+	mixer.addEventListener('loop', onLoopFinished);
 
-  function onLoopFinished(event) {
-    if (event.action === startAction) {
-      mixer.removeEventListener("loop", onLoopFinished);
+	function onLoopFinished(event) {
+		if (event.action === startAction) {
+			mixer.removeEventListener('loop', onLoopFinished);
 
-      executeCrossFade(startAction, endAction, duration);
-    }
-  }
+			executeCrossFade(startAction, endAction, duration);
+		}
+	}
 }
 
 function executeCrossFade(startAction, endAction, duration) {
-  // Not only the start action, but also the end action must get a weight of 1 before fading
-  // (concerning the start action this is already guaranteed in this place)
+	// Not only the start action, but also the end action must get a weight of 1 before fading
+	// (concerning the start action this is already guaranteed in this place)
 
-  if (endAction) {
-    setWeight(endAction, 1);
-    endAction.time = 0;
+	if (endAction) {
+		setWeight(endAction, 1);
+		endAction.time = 0;
 
-    if (startAction) {
-      // Crossfade with warping
+		if (startAction) {
+			// Crossfade with warping
 
-      startAction.crossFadeTo(endAction, duration, true);
-    } else {
-      // Fade in
+			startAction.crossFadeTo(endAction, duration, true);
+		} else {
+			// Fade in
 
-      endAction.fadeIn(duration);
-    }
-  } else {
-    // Fade out
+			endAction.fadeIn(duration);
+		}
+	} else {
+		// Fade out
 
-    startAction.fadeOut(duration);
-  }
+		startAction.fadeOut(duration);
+	}
 }
 
 // This function is needed, since animationAction.crossFadeTo() disables its start action and sets
 // the start action's timeScale to ((start animation's duration) / (end animation's duration))
 
 function setWeight(action, weight) {
-  action.enabled = true;
-  action.setEffectiveTimeScale(1);
-  action.setEffectiveWeight(weight);
+	action.enabled = true;
+	action.setEffectiveTimeScale(1);
+	action.setEffectiveWeight(weight);
 }
 
 function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
 
-  renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 function animate() {
-  // Render loop
+	// Render loop
 
-  requestAnimationFrame(animate);
+	requestAnimationFrame(animate);
 
-  for (let i = 0; i !== numAnimations; ++i) {
-    const action = allActions[i];
-    const clip = action.getClip();
-    const settings = baseActions[clip.name] || additiveActions[clip.name];
-    settings.weight = action.getEffectiveWeight();
-  }
+	for (let i = 0; i !== numAnimations; ++i) {
+		const action = allActions[i];
+		const clip = action.getClip();
+		const settings = baseActions[clip.name] || additiveActions[clip.name];
+		settings.weight = action.getEffectiveWeight();
+	}
+	if (myOwnActions.showTriggerPoints) {
+		triggerPoints.forEach((mesh) => {
+			mesh.visible = true;
+		});
+	} else {
+		triggerPoints.forEach((mesh) => {
+			mesh.visible = false;
+		});
+	}
 
-  // Get the time elapsed since the last frame, used for mixer update
+	// Get the time elapsed since the last frame, used for mixer update
 
-  const mixerUpdateDelta = clock.getDelta();
+	const mixerUpdateDelta = clock.getDelta();
 
-  // Update the animation mixer, the stats panel, and render this frame
+	// Update the animation mixer, the stats panel, and render this frame
 
-  mixer.update(mixerUpdateDelta);
+	mixer.update(mixerUpdateDelta);
 
-  stats.update();
+	stats.update();
 
-  renderer.render(scene, camera);
+	renderer.render(scene, camera);
 }
